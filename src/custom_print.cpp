63a05bd46e171ac09b3c4f1482947c70f484b255
Timur Bikbulatov <57445583+ch1cly@users.noreply.github.com>
2023-10-06 14:59:02 +0300

    Update custom_print.cpp

--- src/custom_print.cpp
+++ src/custom_print.cpp
@@ -1,6 +1,52 @@
-#include "custom_print.h"
+#include "cstack.hpp"
 
-void print_hello_world(int8_t &ref_int) {
-    ref_int++;
-    std::cout << "Hello World\n";
+CStack::~CStack() {
+    clear();
+}
+
+CStack::CStack() :first(nullptr), last(nullptr) {}
+
+void CStack::pop(){
+    if (empty()) return;
+    Node* nd = first;
+    if (first != last) {
+        while (nd->next != last) 
+            nd = nd->next;
+        nd->next = nullptr;
+        delete last;
+        last = nd;
+    } else {
+        delete first;
+    }
+}
+
+void CStack::push(uint32_t val){
+    Node* new_Node = new Node(val);
+    if (empty()) {
+        first = new_Node;
+        last = first;
+        return;
+    }
+    last->next = new_Node;
+    last = new_Node;
+}
+
+uint32_t CStack::top(){
+    if (!empty()) {
+        return last->info;
+    } else {
+        return -1;
+    }
+}
+
+void CStack::clear(){
+    if (empty()) {
+        while(first) {
+            pop();
+        }
+    }
+}
+
+bool CStack::empty(){
+    return first == nullptr;
 }
